d61a457a61866d95322c1b15d50f63c81c4b1c44
Moriarty <seylvin2005@gmail.com>
2024-03-17 20:33:58 +0200

    Implement support of quotation marks as non formating zone - Add support of ``` quotations marks as non formating zone - Add partialy logic to detect nested formating

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ def convert_to_html(markdown_text: str):
 
     while(counter < len(markdown_text)):
 
-        if (counter + 1 == len(markdown_text)):
+        if (counter + 2 == len(markdown_text)):
             return new_text
     
 
         if(markdown_text[counter] == "*" and markdown_text[counter + 1] == "*"):
             new_text += "<b>"
             counter += 2;
+            first_index = counter;
             if(counter >= len(markdown_text)):
                 print("Incorect formating out of range", file=sys.stderr)
                 #print(new_text);
@@ -43,9 +44,17 @@ def convert_to_html(markdown_text: str):
                     if(markdown_text[counter - 1] == " "):
                         print("Invalid formating: no space symbol is allowed right before \'**\'", file=sys.stderr)
                         sys.exit(1)
+
+                    last_index = counter - 1;
+
+                    Contain_Nested_Formating = IsContainNestedFormating(markdown_text,['_','`','```'], first_index, last_index)
+
+                    if(Contain_Nested_Formating):
+                        print("Invalid formating: contain nested formating is not allowed", file=sys.stderr)
+                        sys.exit(1)
+                        
                     new_text += "</b>"
                     counter += 2
-                   # print(new_text)
 
                     if(counter == len(markdown_text)):
                         return new_text
@@ -108,6 +117,46 @@ def convert_to_html(markdown_text: str):
                 print("Invalid formating: no space is allowed right after \'_\'", file=sys.stderr)
                 sys.exit(1)
 
+        elif(markdown_text[counter] == "`" and markdown_text[counter + 1] == "`" and  markdown_text[counter + 2] == "`"):
+            print("i`m in ``` backtics text ")
+            new_text += "<pre>"
+            counter += 3;
+           
+            if(counter >= len(markdown_text)):
+                print("Incorect formating out of range", file=sys.stderr)
+                #print(new_text);
+                #print(counter)
+                sys.exit(1)
+            if(markdown_text[counter] != ' '):
+                if(markdown_text[counter] == "`" and markdown_text[counter + 1] == "`" and  markdown_text[counter + 2] == "`"):
+                    new_text += "</pre>"
+                    counter += 3
+                else:
+                    #print("hello")
+                    
+                    while(markdown_text[counter] != "`" or markdown_text[counter + 1] != "`" or  markdown_text[counter + 2] != "`"):
+
+                        new_text += markdown_text[counter]
+                        counter += 1
+
+                        
+                        if(len(markdown_text)  <= counter + 2):
+                            print("Invalid formating there is no \'```\' at the end", file=sys.stderr)
+                            sys.exit(1)
+
+                    print(new_text)
+                    if(markdown_text[counter - 1] == " "):
+                        print("Invalid formating: no space symbol is allowed right before \'```\'asdasdasdas", file=sys.stderr)
+                        sys.exit(1)
+                    new_text += "</pre>"
+                    counter += 3
+                   # print(new_text)
+
+                    if(counter == len(markdown_text)):
+                        return new_text
+            else:
+                print("Invalid formating: no space symbol is allowed right after \'```\'", file=sys.stderr)
+                sys.exit(1)
 
         elif(markdown_text[counter] == "`"):
             new_text += "<tt>"
@@ -154,7 +203,11 @@ def convert_to_html(markdown_text: str):
 
            # print("out of monospace")
             #print(new_text)
+                
 
+        if(counter == len(markdown_text)):
+            return new_text
+        
         while(markdown_text[counter] == " " or markdown_text[counter] == "\n" or markdown_text[counter] == "\t"):
             new_text += markdown_text[counter]
             counter += 1
@@ -162,10 +215,48 @@ def convert_to_html(markdown_text: str):
             if(counter == len(markdown_text)):
                 return new_text
 
-
     return new_text
 
+def IsContainNestedFormating(text: str, list_of_contained_symbols: list, first_index: int, last_index: int):
 
+    list_of_contained_symbols.sort();
+
+    print(list_of_contained_symbols)
+
+    first, second, third = list_of_contained_symbols;
+    first = list_of_contained_symbols[0];
+    print(first)
+    len_of_largest_format_symbol = len(third);
+
+    first_symbol = text[first_index]
+    last_symbol = text[last_index]
+
+
+    print(f"\n {first_symbol} , {last_symbol}")
+    if(first_symbol != last_symbol):
+        return False
+
+    two_symbols_format = ""
+    three_symbols_format = ""
+    if(len_of_largest_format_symbol == 3):
+        two_symbols_format = text[first_index:first_index + len_of_largest_format_symbol - 1];
+    elif(len_of_largest_format_symbol == 2):
+        three_symbols_format = text[first_index:first_index + len_of_largest_format_symbol - 1];
+    
+    print(list_of_contained_symbols[0])
+    
+
+    if(first_symbol == first):
+        print("first")
+        return True
+    elif(first_symbol == second and last_symbol == second):
+        print("second")
+        return True 
+    elif (first_symbol == third and last_symbol == third):
+        print("thirds")
+        result = True;
+
+    return False;
 
 def read_file(input_path):
     try:
