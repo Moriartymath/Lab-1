a92ffb58623bc846428de24e4820422095995466
Moriarty <seylvin2005@gmail.com>
2024-03-17 09:54:54 +0200

    Add .py file with main logic - Add main logic of converting .md files to .html

---
+++ main.py
@@ -0,0 +1,208 @@
+import sys
+
+def convert_to_html(markdown_text: str):
+
+    counter = 0
+    new_text: str = ""
+
+    while(counter < len(markdown_text)):
+
+        if (counter + 1 == len(markdown_text)):
+            return new_text
+    
+
+        if(markdown_text[counter] == "*" and markdown_text[counter + 1] == "*"):
+            new_text += "<b>"
+            counter += 2;
+            if(counter >= len(markdown_text)):
+                print("Incorect formating out of range", file=sys.stderr)
+                #print(new_text);
+                #print(counter)
+                sys.exit(1)
+            if(markdown_text[counter] != ' '):
+                if(markdown_text[counter] == "*" and markdown_text[counter + 1] == "*"):
+                    new_text += "</b>"
+                    counter += 2
+                else:
+                    #print("hello")
+                    while(markdown_text[counter] != "*" or markdown_text[counter + 1] != "*"):
+
+                        new_text += markdown_text[counter]
+                        #print(new_text)
+                        counter += 1
+
+                        
+                        if(len(markdown_text) - 1 == counter):
+                            print("Invalid formating there is no \'**\' at the end", file=sys.stderr)
+                            sys.exit(1)
+
+                        if(markdown_text[counter - 1] == "*" and markdown_text[counter] != "*"):
+                            print("Invalid formating there is no \'**\' at the end", file=sys.stderr)
+                            sys.exit(1)
+                    
+                    if(markdown_text[counter - 1] == " "):
+                        print("Invalid formating: no space symbol is allowed right before \'**\'", file=sys.stderr)
+                        sys.exit(1)
+                    new_text += "</b>"
+                    counter += 2
+                   # print(new_text)
+
+                    if(counter == len(markdown_text)):
+                        return new_text
+            else:
+                print("Invalid formating: no space symbol is allowed right after \'**\'", file=sys.stderr)
+                sys.exit(1)
+
+        
+        elif(markdown_text[counter] == "_"):
+            new_text += "<i>"
+            counter += 1;
+            if(counter >= len(markdown_text)):
+                print("Incorrect formating: out of range", file=sys.stderr)
+               # print(new_text);
+               # print(counter)
+                sys.exit(1)
+            if(markdown_text[counter] != ' '):
+                if(markdown_text[counter] == "_"):
+                    new_text += "</i>"
+                    counter += 1
+                else:
+                    #print("hello")
+                    IsSnakeCase = False
+                    IsEndOfString = False
+                    while(markdown_text[counter] != "_" or IsSnakeCase):
+
+                        new_text += markdown_text[counter]
+                        #print(new_text)
+                        counter += 1
+                        
+                        if(len(markdown_text) <= counter):
+                            IsEndOfString = True
+                            break
+
+                        #print(counter)
+                        if(len(markdown_text) -  1 == counter):
+                            continue;
+                        
+                        if(markdown_text[counter] == "_" and markdown_text[counter - 1].isalpha() and markdown_text[counter + 1].isalpha()):
+                            IsSnakeCase = True
+                            #print("IsSnakeCase")
+                        else:
+                            IsSnakeCase = False
+                            #print("IsNOTSnakeCase")
+
+                    if(IsEndOfString):
+                        print("Invalid formating: there is no \'_\' at the end", file=sys.stderr)
+                        sys.exit(1)
+                    
+                    if(markdown_text[counter - 1] == " "):
+                        print("Invalid formating: no space symbol is allowed right before \'_\'", file=sys.stderr)
+                        sys.exit(1)
+                    new_text += "</i>"
+                    counter += 1
+                   # print(new_text)
+
+                    if(counter == len(markdown_text)):
+                        return new_text
+            else:
+                print("Invalid formating: no space is allowed right after \'_\'", file=sys.stderr)
+                sys.exit(1)
+
+
+        elif(markdown_text[counter] == "`"):
+            new_text += "<tt>"
+            counter += 1;
+            if(counter >= len(markdown_text)):
+                print("Incorrect formating: out of range", file=sys.stderr)
+               # print(new_text);
+               # print(counter)
+                sys.exit(1)
+            if(markdown_text[counter] != ' '):
+                if(markdown_text[counter] == "`"):
+                    new_text += "</tt>"
+                    counter += 1
+                else:
+                    IsEndOfString = False;
+                    #print(IsEndOfString)
+                    while(markdown_text[counter] != "`"):
+
+                        new_text += markdown_text[counter]
+                       # print(new_text)
+                        counter += 1
+                        #print(counter)
+
+                        
+                        if(len(markdown_text) == counter):
+                            IsEndOfString = True
+                            break;
+                    
+                    if(IsEndOfString):
+                        print("Invalid formating: there is no \'`\' at the end", file=sys.stderr)
+                        sys.exit(1)
+                    if(markdown_text[counter - 1] == " "):
+                        print("Invalid formating: no space symbol is allowed right before \'`\'", file=sys.stderr)
+                        sys.exit(1)
+                    new_text += "</tt>"
+                    counter += 1
+                    #print("out_of_while")
+
+                    if(counter == len(markdown_text)):
+                        return new_text
+            else:
+                print("Invalid formating: no space is allowed right after \'`\'", file=sys.stderr)
+                sys.exit(1)
+
+           # print("out of monospace")
+            #print(new_text)
+
+        while(markdown_text[counter] == " " or markdown_text[counter] == "\n" or markdown_text[counter] == "\t"):
+            new_text += markdown_text[counter]
+            counter += 1
+            
+            if(counter == len(markdown_text)):
+                return new_text
+
+
+    return new_text
+
+
+
+def read_file(input_path):
+    try:
+        with open(input_path, 'r', encoding='utf-8') as file:
+            return file.read()
+    except FileNotFoundError as e:
+        print(f"Error: {e}", file=sys.stderr)
+        sys.exit(1)
+    except Exception as e:
+        print(f"Error: {e}", file=sys.stderr)
+        sys.exit(1)
+
+def save_to_file(html_text, output_path):
+    try:
+        with open(output_path, 'w', encoding='utf-8') as file:
+            file.write(html_text)
+    except Exception as e:
+        print(f"Error: {e}", file=sys.stderr)
+        sys.exit(1)
+
+
+
+if __name__ == "__main__":
+
+    argv_len = len(sys.argv)
+
+    if (argv_len < 2  or argv_len > 3):
+        print("Invalid amount of parametrs", file=sys.stderr)
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    markdown_text = read_file(input_file)
+    html_text = convert_to_html(markdown_text)
+
+    if (argv_len == 3):
+        output_file = sys.argv[2]
+        save_to_file(html_text, output_file)
+        print(f"File was successfuly converted to HTML and saved to {output_file}")
+    else:
+        print(html_text)
